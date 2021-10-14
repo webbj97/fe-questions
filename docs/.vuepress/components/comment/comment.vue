304e286551c6edc68f0479fac8af4b6dfc56fbdf
yugaung <yuguang@gaoding.com>
2021-10-14 11:16:22 +0800

    切分支暂提

--- docs/.vuepress/components/comment/comment.vue
+++ docs/.vuepress/components/comment/comment.vue
@@ -21,7 +21,7 @@ export default {
             admin: ["webbj97"],
             // id 用于当前页面的唯一标识，一般来讲 pathname 足够了，
             // 但是如果你的 pathname 超过 50 个字符，GitHub 将不会成功创建 issue，此情况可以考虑给每个页面生成 hash 值的方法.
-            id: location.pathname.subStr(0, 50),
+            id: location.pathname.substr(0, 50),
             distractionFreeMode: false,
         };
         const gitalk = new Gitalk(commentConfig);
