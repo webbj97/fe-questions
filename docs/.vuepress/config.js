d292d52111a873ac867b8619cc3b226164b214b6
yugaung <yuguang@gaoding.com>
2022-03-23 18:11:04 +0800

    添加看板娘

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,26 @@
 module.exports = {
-    title: "前端闭关",
-    description: "闭关，慢慢来",
+    title: "余光的前端小屋",
+    description: "慢慢来，会很快",
     base: "/fe-questions/", // github仓库名
-    head: [
-        ["link", { rel: "icon", href: "/favicon.ico" }]
-    ],
-    theme: 'antdocs',
+    head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+    theme: "antdocs",
     plugins: [
         ["@vuepress/back-to-top"],
         ["vuepress-plugin-code-copy"],
         ["vuepress-plugin-reading-progress"],
-        'demo-container',
+        [
+            "@vuepress-reco/vuepress-plugin-kan-ban-niang",
+            {
+                theme: ["z16"],
+            },
+        ],
+        "demo-container",
     ],
     themeConfig: {
         repo: "https://github.com/webbj97/fe-questions",
         smoothScroll: true, //平滑滚动
-        lastUpdated: '上次更新于',
-        nav: require('./config/nav'),
-        sidebar: require('./config/sidebar'),
+        lastUpdated: "上次更新于",
+        nav: require("./config/nav"),
+        sidebar: require("./config/sidebar"),
     },
 };
